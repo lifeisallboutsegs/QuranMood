fbc6a20a458bde2f2fc88f1bc9d51f92565149e2
Isai Ivanov <isaiivanov19@gmail.com>
2025-05-28 22:56:54 +0600

    Again some merge conflicts fixed

--- Backend/routes/verseRoutes.js
+++ Backend/routes/verseRoutes.js
@@ -9,39 +9,24 @@ const {
   getAllVerses,
   getVerseById,
   getVersesByTag,
-<<<<<<< HEAD
   getMoods
 } = require("../controllers/verseController");
 
-
+// GET routes
 router.get("/random/:mood", getVerseByMood);
 router.get("/moods", getMoods);
 router.get("/search/tag", getVersesByTag);
 router.get("/all", getAllVerses);
 router.get("/:id", getVerseById);
 
-
+// POST routes
 router.post("/add/:surah/:verse", addVerse);
 router.post("/manual", addVerseManual);
-router.put("/:id", editVerse);
-=======
-} = require("../controllers/verseController");
 
-router.get("/", getVerseByMood);
-
-router.get("/all", getAllVerses);
-
-router.get("/:id", getVerseById);
-
-router.get("/search/tag", getVersesByTag);
-
-router.post("/add/:surah/:verse", addVerse);
-
-router.post("/manual", addVerseManual);
-
+// PUT route
 router.put("/:id", editVerse);
 
->>>>>>> b6f3e98f09a7758e18f56347718c8ca26319f6ba
+// DELETE route
 router.delete("/:id", deleteVerse);
 
 module.exports = router;
